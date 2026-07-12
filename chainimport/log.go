lightninglabs/neutrino

chainimport+log: introduce logging during import

chainimport/log.go
@@ -1,0 +1,25 @@
+package chainimport
+
+import "github.com/btcsuite/btclog"
+
+// log is a logger that is initialized with no output filters.  This
+// means the package will not perform any logging by default until the caller
+// requests it.
+var log btclog.Logger
+
+// log is a logger that is initialized with no output filters. This means the
+// package will not perform any logging by default until the caller requests it.
+func init() {
+	DisableLog()
+}
+
+// DisableLog disables all library log output. Logging output is disabled by
+// default until either UseLogger or SetLogWriter are called.
+func DisableLog() {
+	log = btclog.Disabled
+}
+
+// UseLogger uses a specified Logger to output package logging info.
+func UseLogger(logger btclog.Logger) {
+	log = logger
+}

flokiorg/flokicoin-neutrino

chainimport/log.go
@@ -1,0 +1,25 @@
+package chainimport
+
+import flog "github.com/flokiorg/go-flokicoin/log"
+
+// log is a logger that is initialized with no output filters.  This
+// means the package will not perform any logging by default until the caller
+// requests it.
+var log flog.Logger
+
+// log is a logger that is initialized with no output filters. This means the
+// package will not perform any logging by default until the caller requests it.
+func init() {
+	DisableLog()
+}
+
+// DisableLog disables all library log output. Logging output is disabled by
+// default until either UseLogger or SetLogWriter are called.
+func DisableLog() {
+	log = flog.Disabled
+}
+
+// UseLogger uses a specified Logger to output package logging info.
+func UseLogger(logger flog.Logger) {
+	log = logger
+}